0f3235c139082936c1ca26458a68033d1ede7716
Charlinson <charlinson2013@gmail.com>
2025-01-24 20:55:53 -0500

    feat: agregar clase User para manejar datos de usuario

---
+++ model/user.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(nombre, correo, contrasena, numeroIdetificacion) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contrasena = contrasena;
+        this.numeroIdetificacion = numeroIdetificacion;
+    }
+}
+
+export default User;
